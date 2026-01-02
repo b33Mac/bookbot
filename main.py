72690a1abf2d67967d3860de26e5d874c528f8fc
BMac <brayden.dekleva54321@gmail.com>
2026-01-03 01:11:47 +1100

    tidying up formatting of report

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from stats import get_book_text, count_words, character_count, sort_on, list_of_dictionary
+
 def main():
     words = get_book_text("./books/frankenstein.txt")
     count = character_count(words)
     organised_count = list_of_dictionary(count)
     
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {count_words(words)} total words")
+    print("--------- Character Count -------")
     
-    print(organised_count)
-    #print(count)
-    #print(f"Found {count_words(words)} total words")
+    
+    #for item in organised_count: 
+    #for i in organised_count:
+    #   print(organised_count[i["Char"]])
+
 
 
 main()
