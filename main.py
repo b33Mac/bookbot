c26df7c89ece095d8df077a3dc0d9a610e801fd4
BMac <brayden.dekleva54321@gmail.com>
2026-01-03 12:40:12 +1100

    Finished

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
+import sys
 from stats import get_book_text, count_words, character_count, sort_on, list_of_dictionary
 
 def main():
-    words = get_book_text("./books/frankenstein.txt")
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_file = sys.argv[1]
+    words = get_book_text(f"{book_file}")
     count = character_count(words)
     organised_count = list_of_dictionary(count)
-    
+
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_file}")
     print("----------- Word Count ----------")
     print(f"Found {count_words(words)} total words")
     print("--------- Character Count -------")
     
-    
-    #for item in organised_count: 
-    #for i in organised_count:
-    #   print(organised_count[i["Char"]])
-
-
+    for entry in organised_count:
+        char = entry["char"]
+        num = entry["num"]
+        if char.isalpha() == True:
+            print(f"{char}: {num}")
+        
 
 main()
